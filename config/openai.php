67e50f074694193c5c67181f7eda397ac109aca9
spyder1211 <soichiro.yano.g@gmail.com>
2024-01-15 13:57:00 +0900

    Add OpenAI key and openai-php/client dependency

---
+++ config/openai.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Configuration file for OpenAI.
+ *
+ * This file contains the configuration options for OpenAI integration.
+ * The 'OPENAI_KEY' option is used to specify the API key for OpenAI.
+ * You can set the API key by setting the 'OPENAI_KEY' environment variable.
+ */
+return [
+
+    'OPENAI_KEY' => env('OPENAI_KEY'),
+];
